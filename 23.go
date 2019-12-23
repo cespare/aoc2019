0b3eb71d6df7a99bfcb45e8b18bf6c72e65f542b
Caleb Spare <cespare@gmail.com>
2019-12-23 00:33:25 -0800

    Add day 23 (very hacky)

---
+++ 23.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func init() {
+	addSolutions(23, problem23)
+}
+
+func problem23(ctx *problemContext) {
+	prog := readProg(ctx.f)
+	ctx.reportLoad()
+
+	start := make(chan struct{})
+	nw := newNetwork(50)
+	for addr := int64(0); addr < 50; addr++ {
+		c := nw.addComputer(prog, addr)
+		go func() {
+			<-start
+			c.ic.run()
+			log.Panicf("computer %d halted", c.addr)
+		}()
+	}
+	close(start)
+	nw.monitor()
+}
+
+type network struct {
+	cs      []*netComputer
+	natMu   sync.Mutex
+	nat     *packet
+	lastNAT *packet
+}
+
+func newNetwork(n int) *network {
+	nw := &network{cs: make([]*netComputer, n)}
+	return nw
+}
+
+func (nw *network) monitor() {
+	for {
+		log.Println("Waiting for idle...")
+	idleLoop:
+		for {
+			nw.natMu.Lock()
+			natSet := nw.nat != nil
+			nw.natMu.Unlock()
+			if !natSet {
+				time.Sleep(10 * time.Millisecond)
+				continue
+			}
+			for _, c := range nw.cs {
+				if atomic.LoadInt64(&c.idle) < 1000 {
+					time.Sleep(time.Millisecond)
+					continue idleLoop
+				}
+			}
+			break
+		}
+		nw.natMu.Lock()
+		if nw.nat != nil && nw.lastNAT != nil && nw.nat.y == nw.lastNAT.y {
+			log.Panicln("part 2:", nw.nat.y)
+		}
+		nw.lastNAT = nw.nat
+		nat := nw.nat
+		nw.nat = nil
+		nw.natMu.Unlock()
+		log.Printf("{%d, %d}", nat.x, nat.y)
+		c0 := nw.cs[0]
+		c0.mu.Lock()
+		c0.in = append(c0.in, nat.x, nat.y)
+		c0.mu.Unlock()
+	}
+}
+
+type netComputer struct {
+	nw   *network
+	addr int64
+	mu   sync.Mutex
+	in   []int64
+	out  []int64
+	idle int64
+	ic   *intcode2
+}
+
+func (nw *network) addComputer(prog []int64, addr int64) *netComputer {
+	c := &netComputer{
+		nw:   nw,
+		addr: addr,
+	}
+	nw.cs[addr] = c
+	inFunc := func() []int64 {
+		if addr >= 0 {
+			r := []int64{addr}
+			addr = -1
+			return r
+		}
+		c.mu.Lock()
+		if len(c.in) > 0 {
+			atomic.SwapInt64(&c.idle, 0)
+			r := c.in
+			c.in = nil
+			c.mu.Unlock()
+			return r
+		}
+		c.mu.Unlock()
+		time.Sleep(500 * time.Microsecond)
+		atomic.AddInt64(&c.idle, 1)
+		return []int64{-1}
+	}
+	outFunc := func(v int64) {
+		c.out = append(c.out, v)
+		if len(c.out) < 3 {
+			return
+		}
+		addr, x, y := c.out[0], c.out[1], c.out[2]
+		// log.Printf("computer %d sending {%d, %d} to %d", c.addr, x, y, addr)
+		c.out = c.out[:0]
+		if addr == 255 {
+			fmt.Printf("\033[01;34m>>>> y: %v\x1B[m\n", y)
+			c.nw.natMu.Lock()
+			c.nw.nat = &packet{x, y}
+			c.nw.natMu.Unlock()
+			return
+		}
+		peer := c.nw.cs[addr]
+		peer.mu.Lock()
+		peer.in = append(peer.in, x, y)
+		peer.mu.Unlock()
+	}
+	c.ic = newIntcode2WithMem(prog, addr)
+	c.ic.setCallbackMode(inFunc, outFunc)
+	return c
+}
+
+type packet struct {
+	x int64
+	y int64
+}
